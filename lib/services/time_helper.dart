e1e132fc9d6a70164d1c69da2a35c4b59f7851d1
Erwan Rossignol <erwan@hotmail.ch>
2026-03-31 08:01:53 +0200

    [FEAT]: implement baffeur and dragofesse logic

--- lib/services/time_helper.dart
+++ lib/services/time_helper.dart
@@ -1,5 +1,33 @@
+import 'package:flutter_alarm_clock/flutter_alarm_clock.dart';
+
 int getCaresseurTime(int actualSerenity, int wantedSerenity, int actualCaresseurJauge){
   int j = actualCaresseurJauge;
+  int time = 0;
+  int finalJaugeValue = j - (actualSerenity-wantedSerenity).abs();
+  bool isEnough = finalJaugeValue > 0;
+  if (!isEnough){
+    return -1;
+  }
+  while(j > finalJaugeValue){
+    if (j > 90000){
+      j -= 40;
+    }
+    else if (j > 70000) {
+      j -= 30;
+    }
+    else if (j > 40000){
+      j -= 20;
+    }
+    else {
+      j -= 10;
+    }
+    time += 10;
+  }
+  return time;
+}
+
+int getBaffeurTime(int actualSerenity, int wantedSerenity, int actualBaffeurJauge){
+  int j = actualBaffeurJauge;
   int time = 0;
   bool isEnough = j - (actualSerenity-wantedSerenity).abs() > 0;
   if (!isEnough){
@@ -23,7 +51,39 @@ int getCaresseurTime(int actualSerenity, int wantedSerenity, int actualCaresseur
   return time;
 }
 
+int getDragofesseTime(int actualLove, int actualDragofesseJauge){
+  int j = actualDragofesseJauge;
+  int time = 0;
+  bool isEnough = j - (actualLove-20000).abs() > 0;
+  if (!isEnough){
+    return -1;
+  }
+  while(j > j - (actualLove-20000).abs()){
+    if (j > 90000){
+      j -= 40;
+    }
+    else if (j > 70000) {
+      j -= 30;
+    }
+    else if (j > 40000){
+      j -= 20;
+    }
+    else {
+      j -= 10;
+    }
+    time += 10;
+  }
+  return time;
+}
+
+
 int getXpLevel (int actualLevel, int actualXp, int actualJauge){
 
   return 0;
+}
+
+bool createTimer(int seconds, String title){
+  String tmp = title.isNotEmpty ? title : "Dragodinde Timer";
+  FlutterAlarmClock.createTimer(length: seconds, title: tmp);
+  return true;
 }
